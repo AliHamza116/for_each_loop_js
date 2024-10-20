fc2f9ed697c501d312c355ab0de1fb2c4aafc1cf
AliHamza116 <142170084+AliHamza116@users.noreply.github.com>
2024-10-20 11:12:40 -0700

    Add files via upload

---
+++ for each loop in array/script.js
@@ -0,0 +1,12 @@
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+
+// arr.forEach(function(val,idx,arr){
+//     val=val*val;
+//     console.log(val,idx,arr);
+// });
+let arr=[1,2,3,4,5,6,7,8,9,10];
+
+arr.forEach((val,idx,arr)=>{
+    val=val*val;
+    console.log(val,idx,arr);
+});
